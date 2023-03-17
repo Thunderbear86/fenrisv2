c70eec7207f4b070225bc347b974005c739fc431
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-03-17 18:15:48 +0100

    Portfolio grid test

--- design.php
+++ design.php
@@ -1,1 +1,148 @@
-<?php
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Fenris - Kontakt</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Thorbjørn 'Thunderbear' Wagner">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/ef424bfb92.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="port">
+
+
+<nav>
+    <?php include 'menu.php';?>
+</nav>
+
+<main>
+
+    <!-- Gallery -->
+    <div class="row">
+        <!-- 1 AI & Foto -->
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+
+            <img
+                    src="img/port/viking.png"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="viking"
+            />
+
+            <img
+                src="img/car/car3.png"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="AI Wolf 1"
+            />
+
+            <img
+                src="img/bg3.png"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="AI Wolf 2"
+            />
+
+
+        </div>
+
+        <!-- 2 vector realisme -->
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+
+            <img
+                    src="img/port/Plakat1.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Plakat"
+            />
+
+            <img
+                    src="img/car/car1.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Lady In Pink"
+            />
+
+            <img
+                    src="img/port/t2.png"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Tounge out 2"
+            />
+
+            <img
+                    src="img/port/tof14.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Tounge out"
+            />
+
+            <img
+                src="img/car/car2.png"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Strongman Warrior"
+            />
+
+        </div>
+
+        <!-- 3 vector cartoon -->
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+
+            <img
+                src="img/Pickle Rick-01.jpg"
+                class="w-100 shadow-1-strong rounded mb-4"
+                alt="Pickle Rick"
+            />
+
+            <img
+                    src="img/Fabeldyr bæltedyr-01.png"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Bæltedyr"
+            />
+
+            <img
+                    src="img/opgave%2021%2011%2022-01.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Drage"
+            />
+
+            <img
+                    src="img/port/sword.png"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Sværd"
+            />
+
+            <img
+                    src="img/hotdogs-01.png"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Hotdogs"
+            />
+
+        </div>
+
+
+    </div>
+    <!-- Gallery -->
+
+    <footer>
+        <?php include 'footer.php';?>
+    </footer>
+
+</main>
+
+
+<!-- Java Menu -->
+<script src="js/main.js"></script>
+
+<!-- Bootstrap -->
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+</body>
+</html>
+
