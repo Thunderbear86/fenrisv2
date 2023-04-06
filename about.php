8332fd3bc6ff2b8668033ae14d126c3c97b6335f
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-04-06 14:59:36 +0200

    Om punkter udfyldt

--- about.php
+++ about.php
@@ -44,12 +44,19 @@
             </div>
 
             <div class="col bio">
-                <h2 class="sec-text">BIO</h2>
                 <ul>
-                    <li>Interesser og Erfaring</li>
-                    <li>Multimediedesigner</li>
-                    <li>Social- & Specialpædagog</li>
-                    <li>Eventsikkerhed & Personbeskyttelse</li>
+                    <h2 class="sec-text">En alsidig generalist</h2>
+                    <p class="fw-light">Som autodidaktisk generalist, har jeg nu fundet mig tilrette i en
+                        verden af <span class="fw-bold">design</span> og <span class="fw-bold">udvikling</span>.</p>
+
+                    <br>
+
+                    <p class="fw-light">Med viden og praksiserfaring fra diverse brancher, såsom event,
+                        personbeskyttelse samt social- og specialpædagogik, er jeg en alsidig aspirerende <span class="fw-bold">multimediedesigner</span>.</p>
+
+                    <br>
+
+                    <p class="fw-light">Min største styrke er min evne til at finde <span class="fw-bold">løsninger</span>, ofte med en innovativ tilgang til opgaven.</p>
                 </ul>
             </div>
 
@@ -60,10 +67,16 @@
                     <div class="col-md-4">
                         <div class="job job-1 py-2 px-3 rounded-bottom border-bottom-3 d-flex flex-column h-100">
                             <h3>Webdesign</h3>
+                            <h4 class="sec-text">Dit behov - Dit valg</h4>
                             <ul>
-                                <li>Unik Specialdesignet Hjemmeside</li>
+                                <li>En unik specialdesignet hjemmeside</li>
+                                    <p class="fw-light pt-1">Hvilket betyder fuld kontrol over alle elementer, kodet fra første linje.</p>
                                 <li>Wordpress Hjemmeside</li>
+                                    <p class="fw-light pt-1">Hjemmesiden er hurtigere at bygge, da den er opbygget af blokelementer.
+                                    Efterfølgende er det nemmere selv at vedligeholde.</p>
                                 <li>Vedligeholdelse & Opdatering</li>
+                                    <p class="fw-light pt-1">Du får hjælp til at opadtere, opgradere,
+                                    re-designe eller generel vedligeholdelse af din hjemmeside.</p>
                             </ul>
                         </div>
                     </div>
@@ -73,20 +86,39 @@
                             <h3>Grafisk Design</h3>
                             <ul>
                                 <li>AI Generering</li>
-                                <li>Fotokomposition</li>
+                                    <p class="fw-light pt-1">Kunstig intelligens tager i 2023 kvantespring og brager
+                                        også frem, på det grafiske område!</p>
+
+                                    <p class="fw-light pt-1">Midjourney er en af de store spillere, hvor der med efterbehandling, kan skabes fantastiske billeder.
+                                    Jeg har været med fra beta-test og udvider løbende mine evner i AI generering.</p>
+                                <li>Billedekomposition</li>
+                                    <p class="fw-light pt-1">Kunsten i, at sammensætte forskellige billeder og skab en fornemmelse af, at de altid har hørt sammen.</p>
                                 <li>Vector</li>
-                                <li>Foto</li>
+                                    <p class="fw-light pt-1">Grafisk design rummer alt fra logo til kreation af figurer. Alt er tegnet digitalt i hånden og
+                                        giver mulighed for gennemgribende ændringer, på ingen tid.</p>
+                                <li>Billederedigering</li>
+                                    <p class="fw-light pt-1">Få dine billeder redigeret i en klassisk, trendy eller unik stil.
+                                        I Photoshop er det ofte kun fantasien der sætter grænser.</p>
                             </ul>
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <div class="job job-3 py-2 px-3 rounded-bottom border-bottom-3 d-flex flex-column h-100">
-                            <h3>Foto</h3>
+                            <h3>Fotografering</h3>
                             <ul>
-                                <li>Klassisk</li>
-                                <li>Alternativ</li>
-                                <li>Kunst</li>
+                                <li>Produk</li>
+                                    <p class="fw-light pt-1">Fotografering af produkter til din hjemmeside, app eller markedsføring.</p>
+                                <li>Portræt</li>
+                                    <p class="fw-light pt-1">Portrætfotografering kan deles op i to: </p>
+                                    <p class="fw-light pt-1">Fotografering af medarbejdere, til hjemmeside eller intern brug.</p>
+                                    <p class="fw-light pt-1">Kreativ portrætfotografering.</p>
+                                <li>Boudoir</li>
+                                    <p class="fw-light pt-1">Kunsten i, at fange et intimt og personligt øjeblik - krydret med en følelse af længsel, passion eller begær</p>
+                                <li>Cosplay</li>
+                                    <p class="fw-light pt-1">Begrebet udklædning gør er ikke i nærheden af, hvad cosplay indeholder.
+                                    Indtag en persona, lev dig ind i rollen for en stund og få et eventyrligt produkt retur.</p>
+
                             </ul>
                         </div>
                     </div>
